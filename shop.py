30a3879bd4cb1a5c59e6b3a7bbd878dfc7544217
codeabuu <abdulngui718@gmail.com>
2023-09-24 15:43:34 -0700

    automatioted shopping list script

--- shop.py
+++ shop.py
@@ -8,22 +8,29 @@ from config import TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN, FROM_PHONE_NUMBER, TO_
 client = Client(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN)
 
 # Define a list of items for your weekly shopping list
-my_weekly_list = [
-        'sugar','delmonte','maize floor','milk','mineral water'
-        ]
+my_weekly_list = []
 
 # Define a function to send a text message with the provided list of items
 def text_list(lst):
     msg_body = '\n'.join(lst)# Join the list items into a single message
+
     message = client.messages.create(
             from_ = FROM_PHONE_NUMBER,#your twilio phone no
             body = msg_body,
             to = TO_PHONE_NUMBER#recipients phine number
             )
+
 # Continuously run the following code
 while True:
-    if datetime.datetime.now():
-        print("Here is your list!")
-        # Send the shopping list via text message using the text_list function
-        text_list(my_weekly_list)
-        time.sleep(86400)
+    enter_list = input("Enter item to add to your list(or 'done' to send): ").strip()
+
+    if enter_list.lower() == 'done':
+        if my_weekly_list:
+            print("Here is your list!")
+            text_list(my_weekly_list)
+            time.sleep(120)
+            my_weekly_list = []# Clear the shopping list after sending
+        else:
+            print("Shopping list empty")
+    else:
+        my_weekly_list.append(enter_list)
